dd83824fe5a13863ab98fe81b38ab7ce2417cc52
Alexis Hennecy <alexishennecy@users-MacBook-Pro.local>
2017-10-16 11:14:06 -0500

    updated changes to factory.js

--- about-factory.js
+++ about-factory.js
@@ -1,5 +1,38 @@
-const mission = {
-    "text": "Jackel Audio is a full service audio company with skill working with major names and events in the music industry. From large to small events Jackel Audio has you covered with your best interests in mind. Our team is some of the most higly trained and well versed in the business. Whether you need to rent for one show, purchase equipment for a tour or you need a system to be designed and installed, check with us to see how we can best tend to your needs.",
-    "title": "About Us"
+const ourTeam = {
+    "name": "Courtney",
+    "name": "Tyler",
+    "name": "Deanna",
+    "name": "Ryan",
+    "mame": "Alexis",
+    "title": "Our Team",
 }
+
+
+const weProvide = {
+    "text": "Rental for events",
+    "text": "Touring",
+
+}
+
+
+const reviews = {
+    "text": "I can't speak highly enough about the attentive staff at Jackel Audio. They were so helpful in a last minute order for some extra mics and speakers for a tour date that was thrown on the band last minute. They came through with providing all of the equipment needed and I would recommend them to anyone in the industry",
+    "text": "",
+}
+
+
+const aboutContents = {
+"employees": employees,
+"weProvide": weProvide,
+"reviews": reviews   
+}
+
+
+const location = {
+"location": "4805 Livernois Ave",
+"city": "Detroit",
+"state": "MI 48210",
+
+}
+
 
