f26539c58154be38b9dd8a9328d101c5d52a0a3f
Alexis Hennecy <alexishennecy@users-MacBook-Pro.local>
2017-10-16 09:46:46 -0500

    new files created

---
+++ about-factory.js
@@ -0,0 +1,5 @@
+const mission = {
+    "text": "Jackel Audio is a full service audio company with skill working with major names and events in the music industry. From large to small events Jackel Audio has you covered with your best interests in mind. Our team is some of the most higly trained and well versed in the business. Whether you need to rent for one show, purchase equipment for a tour or you need a system to be designed and installed, check with us to see how we can best tend to your needs.",
+    "title": "About Us"
+}
+
