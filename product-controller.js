0304c515ac514d3c96b52cc097d186dce18cfce1
Tyler Bowman <tylerbowman@Tylers-MacBook-Pro.local>
2017-10-17 14:27:35 -0500

    added page functionality to product page

--- product-controller.js
+++ product-controller.js
@@ -1,4 +1,85 @@
+let current = 0
 
+
+
+function filter(event) {
+    const storedProduct=JSON.parse(localStorage.getItem("productsArchive"));
+    let productsOut = document.getElementById("productList");
+    let start;
+    if(event.type === "click"){
+         start = parseInt(event.target.id);
+    } else {
+         start = event
+    }
+    current=start;
+    productsOut.innerHTML = ""
+    console.log(start)
+    for (let i= start; i < (start+4); i++) {
+        console.log(i);
+        product = storedProduct[i];
+        productsOut.innerHTML += 
+        `
+        <section class= "products ${product.tag}">
+        <img src="${product.img}" width="150px">
+        <ul>
+            <li>Product: ${product.name}</li>
+            <li>Price: $${product.price}</li>
+            <li>Description: ${product.description}</li>
+        </ul>
+        </section>
+        `
+    }
+}
+
+
+
+
+function prevFilter(){
+    switch(true) {
+    case current === 4:
+    filter(0);
+    break;
+case current === 8:
+    filter(4);
+    break;
+case current === 12:
+    filter(8);
+    break;
+case current === 16:
+    filter(12);
+    break;
+}
+}
+
+function nextFilter(){
+    switch (true) {
+        case current === 0:
+            filter(4);
+            break;
+        case current === 4:
+            filter(8);
+            break;
+        case current === 8:
+            filter(12);
+            break;
+        case current === 12:
+            filter(16);
+            break;
+    
+
+}
+}
+document.getElementById("prev").addEventListener("click", prevFilter)
+document.getElementById("0").addEventListener("click", filter)
+document.getElementById("4").addEventListener("click", filter)
+document.getElementById("8").addEventListener("click", filter)
+document.getElementById("12").addEventListener("click", filter)
+document.getElementById("16").addEventListener("click", filter)
+document.getElementById("next").addEventListener("click", nextFilter)
+
+
+filter(0)
+/* 
 //Created a variable to hold the queried DOM element.
 let productsOut = document.getElementById("productList");
 
@@ -14,66 +95,67 @@ for (let i = 0; i < storedProducts.length; i++) {
         <section class="products ${products.tag}" style="display: block" id="${products.title}">
             <img src="${products.img}" width="150px">
             <ul>
-                <p>Product: ${products.name} </p>
-                <p>Price: $${products.price}</p>
-                <p>Description: ${products.description}</p>
+                <li>Product: ${products.name} </li>
+                <li>Price: $${products.price}</li>
+                <li>Description: ${products.description}</li>
             </ul>
         </section>
         `
-    }
-
+    } */
+    
+// Pagination filter
 
 //Filter function
-function filter() {
-    let sale = document.querySelectorAll(".onSale")
-    let unaffordable = document.querySelectorAll(".unaffordableItem")
-    let neat = document.querySelectorAll(".neatItem")
-    let selected = document.getElementById("filter").value
-    if (selected === "onSale") {
-        for (var i = 0; i < sale.length; i++){
-            sale[i].style.display = "block"
-        } 
-        for (var i = 0; i < unaffordable.length; i++){
-            unaffordable[i].style.display = "none"
-        } 
-        for (var i = 0; i < neat.length; i++){
-            neat[i].style.display = "none"
-        }
-    } else if (selected === "unaffordableItem") {
+// function filter() {
+//     let sale = document.querySelectorAll(".onSale")
+//     let unaffordable = document.querySelectorAll(".unaffordableItem")
+//     let neat = document.querySelectorAll(".neatItem")
+//     let selected = document.getElementById("filter").value
+//     if (selected === "onSale") {
+//         for (var i = 0; i < sale.length; i++){
+//             sale[i].style.display = "block"
+//         } 
+//         for (var i = 0; i < unaffordable.length; i++){
+//             unaffordable[i].style.display = "none"
+//         } 
+//         for (var i = 0; i < neat.length; i++){
+//             neat[i].style.display = "none"
+//         }
+//     } else if (selected === "unaffordableItem") {
 
-        for (var i = 0; i < sale.length; i++){
-            unaffordable[i].style.display = "block"
-        } 
-        for (var i = 0; i < sale.length; i++){
-            sale[i].style.display = "none"
-        } 
-        for (var i = 0; i < neat.length; i++){
-            neat[i].style.display = "none"
-        }
-    } else if (selected === "neatItem") {
+//         for (var i = 0; i < sale.length; i++){
+//             unaffordable[i].style.display = "block"
+//         } 
+//         for (var i = 0; i < sale.length; i++){
+//             sale[i].style.display = "none"
+//         } 
+//         for (var i = 0; i < neat.length; i++){
+//             neat[i].style.display = "none"
+//         }
+//     } else if (selected === "neatItem") {
     
-        for (var i = 0; i < neat.length; i++){
-            neat[i].style.display = "block"
-        } 
-        for (var i = 0; i < unaffordable.length; i++){
-            unaffordable[i].style.display = "none"
-        } 
-        for (var i = 0; i < sale.length; i++){
-            sale[i].style.display = "none"
-        }
-    } else if (selected === "all") {
+//         for (var i = 0; i < neat.length; i++){
+//             neat[i].style.display = "block"
+//         } 
+//         for (var i = 0; i < unaffordable.length; i++){
+//             unaffordable[i].style.display = "none"
+//         } 
+//         for (var i = 0; i < sale.length; i++){
+//             sale[i].style.display = "none"
+//         }
+//     } else if (selected === "all") {
         
-        for (var i = 0; i < neat.length; i++) {
-            neat[i].style.display = "block"
-        } 
-        for (var i = 0; i < unaffordable.length; i++){
-            unaffordable[i].style.display = "block"
-        } 
-        for (var i = 0; i < sale.length; i++){
-            sale[i].style.display = "block"
-        }
-    }
-}
+//         for (var i = 0; i < neat.length; i++) {
+//             neat[i].style.display = "block"
+//         } 
+//         for (var i = 0; i < unaffordable.length; i++){
+//             unaffordable[i].style.display = "block"
+//         } 
+//         for (var i = 0; i < sale.length; i++){
+//             sale[i].style.display = "block"
+//         }
+//     }
+// }
 
 
 
--- product-factory.js
+++ product-factory.js
@@ -53,8 +53,162 @@ let djController = {
     "tag": "onSale"
 }
 
+let mixerBehringer = {
+    "name": "Behringer X32 Digital Mixer",
+    "price": "1999.99",
+    "description": "The Behringer X32 digital mixer packs everything you need to easily mix and record a live show into a compact, roadworthy chassis.",
+    "URL": "https://www.sweetwater.com/store/detail/X32?mrkgcl=28&mrkgadid=2945987365&rkg_id=h-8423169d07cf1eb687bd20d11a86cfcb_t-1508257170&product_id=X32&campaigntype=shopping&campaign=aaShopping%2520-%2520Core&adgroup=Live%2520Sound%2520%26%2520Lighting%2520-%2520Mixers%2520-%2520Behringer&placement=google&adpos=1o2&creative=55678035481&device=c&matchtype=&network=g&gclid=Cj0KCQjw1JbPBRCrARIsAOKj2PneIH4FS7g4AoFCA7pqejD0T-8Dk-Ekzr_WiJs_spvgD6SsJd37iLUaAiG6EALw_wcB",
+    "img": "behringer x32.jpg",
+    "title": "mixer",
+    "tag": "onSale"
+}
+
+let mixerPresonus = {
+    "name": "PreSonus StudioLive 32 Series III ",
+    "price": "2999.99",
+    "description": "The PreSonus StudioLive 32 digital mixing console/recorder represents the third generation of the acclaimed StudioLive series, and it's just as capable in the studio as it is for live sound.",
+    "URL": "https://www.sweetwater.com/store/detail/SLMAD32?mrkgcl=28&mrkgadid=3208894243&rkg_id=h-8423169d07cf1eb687bd20d11a86cfcb_t-1508258217&product_id=SLMAD32&campaigntype=shopping&campaign=aaShopping%2520-%2520Core&adgroup=Live%2520Sound%2520%26%2520Lighting%2520-%2520Mixers%2520-%2520Other&placement=google&adpos=1o4&creative=55674776881&device=c&matchtype=&network=g&gclid=Cj0KCQjw1JbPBRCrARIsAOKj2PkF8y7o9LAGFcNEHQBmoN16UCkF38M8oBP0CbPW8YSrjfgO2TATjNQaAgLnEALw_wcB",
+    "img": "presonus mixer.jpg",
+    "title": "mixer",
+    "tag": "onSale"
+}
+
+let mixerSound = {
+    "name": "Sound Devices CL-9",
+    "price": "2429.99",
+    "description": "The CL-9 Linear Fader Controller is a dedicated mixing surface for the 788T.",
+    "URL": "http://www.fullcompass.com/prod/163403-Sound-Devices-CL-9",
+    "img": "sonar.jpg",
+    "title": "mixer",
+    "tag": "onSale"
+}
+
+let mixerAkai = {
+    "name": "AKAI MIDImix",
+    "price": "99.99",
+    "description": "Akai's MIDImix is a compact USB-MIDI DAW controller that is designed to give you simple and intuitive control over mixing and processing in your DAW of choice.",
+    "URL": "http://www.fullcompass.com/prod/285998-AKAI-MIDImix",
+    "img": "akai.jpg",
+    "title": "mixer",
+    "tag": "onSale"
+}
+
+let mixerMackie = {
+    "name": "Mackie 802VLZ4",
+    "price": "299.99",
+    "description": "The 802VLZ4 8-channel mixer features the proven performance of Mackie's Onyx mic preamps in a desk-friendly, ultra-compact design perfect for professional applications with a lower input need.",
+    "URL": "http://www.fullcompass.com/prod/240177-Mackie-802VLZ4",
+    "img": "mackie.jpg",
+    "title": "mixer",
+    "tag": "onSale"
+}
+
+let pioneerController = {
+    "name": "Pioneer DDJ-SX",
+    "price": "289.99",
+    "description": "Gator GTOURDSPDDJSX Road Case for Pioneer DDJ-SX With Sliding Platform",
+    "URL": "https://www.americanmusical.com/Galleria/2/GAT%20GTOURDSPDDJSX-Large.jpg",
+    "img": "controller.jpg",
+    "title": "controller",
+    "tag": "onSale"
+}
+
+let passiveSpeakers = {
+    "name": "15 Inch 2 Way Passive PA Speakers",
+    "price": "1098.99",
+    "description": "The JBL PRX415M 2 Way PA Speaker is a portable, fifteen-inch two-way speaker system designed for either main sound reinforcement or monitoring applications.",
+    "URL": "https://media.americanmusical.com/ItemImages/Large/JBL%20PRX415M%20PR.jpg",
+    "img": "boseSpeaker.jpg",
+    "title": "speakers",
+    "tag": "onSale"
+}
+
+let boseSpeakers = {
+    "name": "Virtually Invisible 891 in-wall speakers",
+    "price": "599.95",
+    "description": "These rectangular Virtually Invisible® in-wall speakers are engineered with one 7 inch woofer and two strategically positioned 1 tweeters each.",
+    "URL": "https://assets.bosecom/content/dam/Bose_DAM/Web/consumer_electronics/global/products/speakers/virtually_invisible_891_in_wall_speakers/product_silo_images/virtually_invisible_891_EC_hero.psd/jcr:content/renditions/cq5dam.web.600.600.png",
+    "img": "boseInWallSpeaker.jpg",
+    "title": "speakers",
+    "tag": "onSale"
+}
+
+let wirelessBoseSpeakers = {
+    "name": "SoundTouch outdoor wireless system with 151 SE speakers",
+    "price": "778.90",
+    "description": "Our popular wireless outdoor speakers deliver rich sound and clear performance—even in large outside areas.",
+    "URL": "https://assets.bose.com/content/dam/Bose_DAM/Web/consumer_electronics/global/products/speakers/soundtouch_151_SE_outdoor_speaker_system/product_silo_images/st_outdoor_151se_sa5_black_EC1.psd/jcr:content/renditions/cq5dam.web.600.600.png",
+    "img": "speakersBoseSmall.png",
+    "title": "speakers",
+    "tag": "onSale"  
+}
+
+ let fullSpeakerBose = {
+    "name": "A Full Bose F1 PA Speaker Setup",
+    "price": "5352.00",
+    "description": "Two 1,000W Array Loudspeakers, Two 1,000W Powered Subwoofers, and Carrying Bags for each",
+    "URL": "https://media.sweetwater.com/api/i/f-webp__ha-63b8f12cb04b3d25__q-82__hmac-a700f6ac0f1020d649ff5c8324fcd56c39193d67/images/items/750/F1System-large.jpg.auto.webp",
+    "img": "bosef1.jpeg",
+    "title": "speakers",
+    "tag": "onSale"
+}
+
+let microphone = {
+    "name": "Samson Meteor Mic USB Studio Condenser Microphone",
+    "price": "69.99",
+    "description": "TSamson meteor mic USB studio condenser microphone produces high-quality audio recording for any application.",
+    "URL": "https://www.google.com/shopping/product/11859342197776045340?lsf=seller:10048,store:13960113375953198258&prds=oid:1044339786323067090&q=microphone+for+sale+near+me&hl=en&ei=tT_mWei3POuwjwS33IeIDA&lsft=gclid:Cj0KCQjw1JbPBRCrARIsAOKj2PlxeO0zo0H2BqSLIztY2r-PeX_uf74_V6pV20vLixsZhh9ySjCoUsoaAluVEALw_wcB",
+    "img": "microphone.jpeg",
+    "title": "microphone",
+    "tag": "onSale"
+}
+
+let microphoneTwo = {
+    "name": "Samson M10 Karaoke Microphone",
+    "price": "19.99",
+    "description": "The Samson, M10 Karaoke Microphone will have you and your friends sounding better than ever.",
+    "URL": "https://www.target.com/p/samson-m10-karaoke-microphone/-/A-14796575?ref=tgt_adv_XS000000&AFID=google_pla_df&CPNG=PLA_Electronics+Shopping&adgroup=SC_Electronics&LID=700000001170770pgs&network=g&device=c&location=9013191&gclid=Cj0KCQjw1JbPBRCrARIsAOKj2Pno7JxzqPTNCRbZb6pvTqs1W5fa9r-CBw-IzafjzrVADQqYaokauekaAru0EALw_wcB&gclsrc=aw.ds",
+    "img": "microphoneTwo.jpeg",
+    "title": "microphone",
+    "tag": "onSale"
+}
+
+let marshallsAmp = {
+    "name": "Marshalls Amlifier",
+    "price": "$250.00",
+    "description": "The Marshall's Amp is our highest rated amp at Jackel's Audio.",
+    "URL": "https://goo.gl/images/NxkznV",
+    "img": "",
+    "title": "amplifier",
+    "tag": "onSale",
+    "page": "onSale"
+}
+
+ let purpleAmp = {
+    "name": "Winfield's Purple Amplifier",
+    "price": "$50.00",
+    "description": "This is one of our mini amps. It is great for traveling!",
+    "URL": "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjKl7KDm_jWAhWG5YMKHXYNCywQjRwIBw&url=http%3A%2F%2Fwww.rainbowguitars.com%2Fguitar%2Fwinfield%2Fcyclone-amplifier-head-purple%2Fcyclonepurple%2Fwf&psig=AOvVaw2sXYXwjxw_Blm-WcR-8Mnh&ust=1508348729210528",
+    "img": "",
+    "title": "amplifier",
+    "tag": "onSale",
+    "page": "onSale"
+}
+
+ let guitarAmp = {
+    "name": "Guitar Amplifer",
+    "price": "$45.00",
+    "description": " This amplifier is great for muscians. You will have incredible sound quality while playing your instruments",
+    "URL": "https://l6c-acdn2.line6.net/data/6/0a020a3d10fdc590224b50a5a1/image/png/file.png",
+    "img": "",
+    "title": "amplifier",
+    "tag": "onSale",
+    "page": "onSale"
+}
+
+
 // Pushing objects into array
-products.push(speakerSystem, amplifier, headphones, djSampler, djController)
+products.push(speakerSystem, amplifier, headphones, djSampler, djController, mixerBehringer, mixerPresonus, mixerSound, mixerAkai, mixerMackie, pioneerController, passiveSpeakers, boseSpeakers, wirelessBoseSpeakers, fullSpeakerBose, microphone, microphoneTwo, marshallsAmp, purpleAmp, guitarAmp)
 
 //Created a constant variable holding the stringified array
 const productsArchiveString = JSON.stringify(products);
@@ -62,5 +216,4 @@ const productsArchiveString = JSON.stringify(products);
 //Stored the stringified array into LocalStorage.
 localStorage.setItem("productsArchive", productsArchiveString);
 
-//Created a variable to hold the parsed array pulled from LocalStorate
-const storedProducts = JSON.parse(localStorage.getItem("productsArchive"));
+
